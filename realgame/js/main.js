4b410df0eed3ae49ed07ad3ff638e84bb5efdd34
woah <32891419+sagbayan@users.noreply.github.com>
2017-10-23 00:19:01 -0700

    Add files via upload
    working preload

--- realgame/js/main.js
+++ realgame/js/main.js
@@ -1,3 +1,4 @@
+//"use strict";
 /* eslint-env browser*/
 /* jslint browser*/
 
@@ -17,56 +18,70 @@ var Preloader = function () {                       //object that holds and load
     this.domain = "";                               //domain for assets.  will append filename of image in function
     this.imageSrcArray = [];                        //array that will contain strings of domain(filepath)+filename
     this.imageArray = [];                           //array that will contain actual Image() objects to be used by HTML
-    
+    console.log("Preload object called");
     //define variables first before calling this function!
-    this.loadAssets = function () {                 //call this function that will produce filled imageArray with loaded images
-        var i;
-        var progress = this.imageSrcArray.length;   //images are done loading when this is 0
+    this.loadAssets = function (temp) {                 //call this function that will produce filled imageArray with loaded images
+        console.log("loadassets called");
+        var i, progress, assetImage;
+        this.imageSrcArray = temp;
+        progress = this.imageSrcArray.length;   //images are done loading when this is 0
         for (i = 0; i < this.imageSrcArray.length; i += 1) {
-            var assetImage = new Image();
+            assetImage = new Image();
             assetImage.onload = function () {
                 progress -= 1;
                 if (progress <= 0) {
-                    this.loadingstatus();           //trigger loading status
+                    this.loadingstatus;           //trigger loading status
                     this.loadingdone = 1;           //declare loading is done
                 }
             };
             assetImage.src = this.domain + this.imageSrcArray[i];       //define src and alt text for images
             assetImage.alt = this.imageSrcArray[i];
             this.imageArray.push(assetImage);                           //push the image into the actual imageArray to be referenced to
+            console.log("imagearray = " + Main.Preloader.imageArray);
         }
-    }
-}
-
-var getImage = function (string_filename) {                                          //function that will return the Image() file src from preloaded array when needed
-    var e;
-    e = Main.Preloader.imageSrcArray.indexOf(Main.Preloader.domain + string_filename);     //find the index of the image you are looking for by filename
-    return Main.Preloader.imageArray[e];
-}
+    };
+};
 
 /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 Game begins here
 ----------------------------------------------------------*/
 var Main = {};              //define main game object
 
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+helper functions within Main class
+----------------------------------------------------------*/
+var getImage = function (preloaderobject, string_name) {                                          //function that will return the Image() file src from preloaded array when needed
+    var e1, e2, e3;
+    e1 = preloaderobject;
+    e2 = preloaderobject.imageSrcArray;
+    e3 = e2.indexOf(string_name);     //find the index of the image you are looking for by filename
+    console.log(e1.imageArray[e3]);
+    return e1.imageArray[e3];
+};
+
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+Define the main flow of the game!
+----------------------------------------------------------*/
 Main.pipeline = function () {         //this function contains the entire game and is called to start the game
-    "use strict";
+    console.log("pipeline called");
     Main.ready = 0;                    //declare the game not ready to go
     
     /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     Define the method to preload the game
     ----------------------------------------------------------*/
     Main.preload = function () {
+        console.log("preload running");
         Main.Preloader = new Preloader();                 //create object to load assets and resources
         Main.Preloader.domain = "res/images/";              //tell the object where the asset folder is located
-        Main.Preloader.loadingstatus = Main.Init;           //define the callback trigger to the init function.  when it turns 1, it'll activate the init function
-        Main.Preloader.LoadAssets(imageSrcArrayReference);  //provide image source array containing string filepath+filenames
-    }
+        Main.Preloader.loadAssets(imageSrcArrayReference);  //provide image source array containing string filepath+filenames
+        Main.Preloader.loadingstatus = Main.Init();           //define the callback trigger to the init function.  when it turns 1, it'll activate the init function
+    };
     
     /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     Define the method to initialize the game
     ----------------------------------------------------------*/
     Main.Init = function () {         //this function initializes the game.
+        console.log("init called");
         /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         VARIABLES AND FUNCTIONS
         ----------------------------------------------------------*/
@@ -78,6 +93,8 @@ Main.pipeline = function () {         //this function contains the entire game a
         
         /* game state tracking */
         Main.stateMenu = 0;          //what screen is the game on. 0 = mining
+        Main.menuInitialized = 0;    //tells if the menu is initialized or not.  starts at 0.
+        Main.menuObjectArray = [];   //array that holds all the objects in a menu
         
         /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         Mining and shit
@@ -97,44 +114,83 @@ Main.pipeline = function () {         //this function contains the entire game a
         Main.player_mine_strength = 0;      //strength of mining ability (determines how many "mines" per in-game click)
         Main.player_mine_amount = 0;        //amount of cursors (determines how many in-game clicks per real-life click)
         
-        //functions to receive input///////////////////////////////////
-        
+        //define objects to be used in the game
+        Main.Object = function (str_name, str_canvasParentid, str_imagename) {
+            this.name = str_name;                          //identifying class name of object (from CSS)
+            this.canvasIndex = 0;                    //will assign index #.
+            this.initialized = 0;                    //state that tells if the object has been initialized yet.  starts at 0
+            this.canvasParentid = str_canvasParentid;//parent element of the canvas.  tells where canvas should be on html
+            this.imagename = str_imagename;                     //filename of image of object
+            this.image = 0;                          //actual image from getImage function
+            
+            while (this.initialized != 1) {
+                var e1;                                                      //create the canvas          
+                e1 = document.createElement("canvas");
+                e1.className = this.name;
+                document.getElementById(this.canvasParentid).appendChild(e1);       //appendChild canvas to the parent you gave
+                this.canvas = e1;
+                this.image = getImage(Main.Preloader, this.imagename);                              //define the image
+                this.initialized = 1;
+                console.log(this.name," has been initialized");
+            }
+            if (this.initialized === 1) {                                 //if object is initialized...
+                console.log("reeeee");
+                this.draw = function () {
+                    var ctx = this.canvas.getContext("2d");
+                    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                    ctx.drawImage(this.image, 0, 0, this.canvas.width, this.canvas.height);
+                };
+            }
+        };
         
         //functions to process input///////////////////////////////////
         Main.mineOre = function (tier, strength, amount) {                       //the method for clicking ONCE
             var e1, chance_iron, chance_copper;
             e1 = Math.random();                                     //roll a random decimal between 0-1
-            if (tier == 1) {                                        //determine possible ore and probability based on tier of player/miner
-                chance_iron = e1 < .25; 
-                chance_copper = e1 >= .25;    
+            if (tier === 1) {                                        //determine possible ore and probability based on tier of player/miner
+                chance_iron = e1 < 0.25;
+                chance_copper = e1 >= 0.25;
                 if (chance_iron) {                             //if dice roll wins iron
                     Main.material_ore_iron += strength * amount;
-                }
-                else if (chance_copper) {                         //if dice roll wins copper
+                } else if (chance_copper) {                         //if dice roll wins copper
                     Main.material_ore_copper += strength * amount;
                 }
             }
-            update_material_vars();
-        }
-
+        };
         
         /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-        GRAPHICS
+        Menu initialization functions
         ----------------------------------------------------------*/
+        Main.buildMenu = function (statemenu) {             //function to initialize objects for a menu.  Only call if menuinitialized = 0
+            Main.menuTotalObjects = 0;                       //total amount of objects needed to be loaded on a given menu
+            Main.menuObjectArray = [];                      //clear the menu object array
+            if (statemenu === 0) {                              //if requested to initialize mining menu...
+                Main.menuTotalObjects = 1;                  //number of objects that need to be loaded for this specific menu
+                var e1 = new Main.Object("rock", "StageRight_1", "rock.png");       //assign each variable to a different object
+                if (e1.initialized) {
+                    Main.menuObjectArray.push(e1);          //put the object into the menuobject array
+                }
+                if (Main.menuObjectArray.length === Main.menuTotalObjects) {    //if all necessary objects are loaded into array
+                    Main.menuInitialized = 1;               //declare menu is initialized    
+                }
+                
+            }
+        };
         
         /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         COMPLETE INITIALIZATION
         ----------------------------------------------------------*/
         Main.ready = 1;             //declare the game ready to go
-        //Main.Loop();               //start the main game loop
-        //Main.Render();             //start the rendering loop
+        Main.Loop();               //start the main game loop
+        Main.Render();             //start the rendering loop
     };
     
     /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     Define the method to receive and process input
     ----------------------------------------------------------*/
     Main.Engine = function () {     //this function listens for input and then processes it to change variables
-        // listen for input
+        // listen for inputs
+        
         // call functions to change variables based on inputs
         // reset inputs
         Main.TickEngine += 1;       //add to tick count
@@ -146,7 +202,20 @@ Main.pipeline = function () {         //this function contains the entire game a
     Main.Render = function () {     //this function renders the game
         // do rendering functions
         // Main.drawbackgrounds()
-        Main.TickRender += 1;        // add to tick count
+        //Draw object canvases
+        if (Main.stateMenu === 0) {         //game is on the mining screen
+            if (!Main.menuInitialized) {            //if menu is not initialize, initialize it
+                Main.buildMenu(0);
+            } else {                                //if it is, then perform the draw function on all objects
+                var i;
+                for (i = 0; i < Main.menuObjectArray.length; i += 1) {
+                    Main.menuObjectArray[i].draw();
+                }
+            }
+        }
+        //console.log(Main.menuObjectArray[0].draw);
+        Main.tickRender += 1;        // add to tick count
+        window.requestAnimationFrame(Main.Render);
     };
     
     /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -156,17 +225,20 @@ Main.pipeline = function () {         //this function contains the entire game a
         Main.Engine();              //run engine
         //add time compensation methods if needed
         Main.TickLoop += 1;         //add to tick count
-        setTimeout(Main.Loop, 1000 / Main.Framerate);     //declare the loop to repeat, at interval equal to length of one game frame (inverse of framerate)
+        setTimeout(Main.Loop, 1000 / Main.framerate);     //declare the loop to repeat, at interval equal to length of one game frame (inverse of framerate)
     };
 };
 
 /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 Game is called here!
 ----------------------------------------------------------*/
-Main.Pipeline();                    //boot that shit up
+Main.pipeline();                    //boot that shit up
+console.log("started");
 
 window.onload = function () {
+    console.log("window.onload called");
     if (!Main.ready) {
-        Main.Preload;
+        Main.preload();
+        console.log("preload called", Main.ready);
     }
-}
+};
